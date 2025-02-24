de7e9fb6096d873d1826bb0ec05059fc7c5d6af1
ElmeroJean <jimenezjean703@gmail.com>
2025-02-24 17:39:59 -0600

    Aprendi un poco de input

---
+++ input.py
@@ -0,0 +1,8 @@
+#Usar con capitalize 
+Nombres=input('Ingrese sus nombres ')
+Apellidos=input('Ingrese sus apellidos ')
+
+Nombres=Nombres.capitalize()
+Apellidos=Apellidos.capitalize()
+
+print(f'Lamentablemente este nombre no es correcto, ingrese su nombre correcto porfavor {Nombres} {Apellidos}')
---
+++ practicaconint.py
@@ -0,0 +1,8 @@
+#Hacer una funcion con input 
+Nombres=input('Ingrese nombres completo Ingeniero ')
+
+
+Nombres=Nombres.title()
+
+
+print(f'Para finalizar le damos las gracias por completar sus Nombres {Nombres}')
